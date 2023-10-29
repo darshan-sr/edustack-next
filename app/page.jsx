cb3f40ac770c9eb9a91a75a64a9e228cbd28c875
Darshan Gowda <darshansr1618@gmail.com>
2023-10-29 19:03:05 +0530

    home page

--- app/page.jsx
+++ app/page.jsx
@@ -12,8 +12,8 @@ const HomePage = () => {
                 <h1 style={{ fontSize: '25px', padding: '10px' }}> | </h1>
                 <img src='/logorv.png' style={{ maxWidth: '60px', maxHeight: '60px', alignItems: 'center' }} />
               </div>
-              <h1 style={{ fontSize: '30px', fontWeight: 'bolder', margin: '25px', fontFamily: 'golos text', fontWeight: '600' }}>Welcome to EduStack for RVITM</h1>
-              <p style={{ margin: '25px', color: 'grey', fontFamily: 'sans-serif', marginBottom: '15vh'}}>A Platform Built to Simplify Attendance Tracking & Academics </p>
+              <h1 style={{ fontSize: '30px', fontWeight: 'bolder', margin: '20px', fontFamily: 'golos text', fontWeight: '600' }}>Welcome to EduStack for RVITM</h1>
+              <p style={{ margin: '20px', color: 'grey', fontFamily: 'sans-serif', marginBottom: '15vh'}}>A Platform Built to Simplify Attendance Tracking & Academics </p>
 
               <div className={styles.buttonContainer}>
               <Link href='/accounts/signin' style={{width: '95%'}}><button className={styles.primaryButton}>Sign In </button></Link>
