5ee6254cfcbbf872676479ac934dddfd91a7397a
Dhyaan1 <65714425+Dhyaan1@users.noreply.github.com>
2023-10-30 15:42:02 +0530

    Error fixing and optimizing Image rendering

--- app/accounts/signin/page.jsx
+++ app/accounts/signin/page.jsx
@@ -1,21 +1,22 @@
-'use client';
-import Link from 'next/link';
-import styles from './SignIn.module.css'
-import { FiMail, FiEye, FiEyeOff } from 'react-icons/fi';
-import { HiOutlineMail , HiOutlineLockClosed} from 'react-icons/hi'
-import { useState } from 'react';
-import { MdArrowBackIosNew } from 'react-icons/md'
-import { signInWithEmailAndPassword, signOut } from 'firebase/auth';
-import { auth, db } from '@/app/firebase/firebase';
-import { Alert } from 'react-bootstrap';
-import { useRouter } from 'next/navigation';
-import { doc, getDoc } from 'firebase/firestore';
- 
+"use client";
+import Link from "next/link";
+import styles from "./SignIn.module.css";
+import { FiEye, FiEyeOff } from "react-icons/fi";
+import { HiOutlineMail, HiOutlineLockClosed } from "react-icons/hi";
+import { useState } from "react";
+import { MdArrowBackIosNew } from "react-icons/md";
+import { signInWithEmailAndPassword, signOut } from "firebase/auth";
+import { auth, db } from "@/app/firebase/firebase";
+import { Alert } from "react-bootstrap";
+import { useRouter } from "next/navigation";
+import { doc, getDoc } from "firebase/firestore";
+import Image from "next/image";
+
 const SignIn = () => {
   const [passwordShown, setPasswordShown] = useState(false);
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
-  const [error, setError] = useState('');
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState("");
   const [isLoading, setIsLoading] = useState(false);
   const router = useRouter();
 
@@ -26,38 +27,35 @@ const SignIn = () => {
 
   const handleSubmit = async (e) => {
     e.preventDefault();
-    setError('');
+    setError("");
     setIsLoading(true); // Set loading state to true
 
     try {
       await signInWithEmailAndPassword(auth, email, password)
         .then(async (res) => {
           const user = res.user;
-          const getRef = doc(db , 'users' , user.uid);
+          const getRef = doc(db, "users", user.uid);
           const userDoc = await getDoc(getRef);
-          try{
-            if(userDoc.exists()){
+          try {
+            if (userDoc.exists()) {
               const userData = userDoc.data();
-              if(userData.type === 'student'){           
-                router.push('/student/home');
-                setIsLoading(false); 
-              }
-              else if(userData.type === 'faculty'){
-                router.push('/faculty/home');
+              if (userData.type === "student") {
+                router.push("/student/home");
                 setIsLoading(false);
-              }
-              else if(userData.type === 'parent'){
-                router.push('/parent/home');
+              } else if (userData.type === "faculty") {
+                router.push("/faculty/home");
                 setIsLoading(false);
-              }
-              else{
-                setError('No Records Found');
+              } else if (userData.type === "parent") {
+                router.push("/parent/home");
+                setIsLoading(false);
+              } else {
+                setError("No Records Found");
                 await signOut(auth);
                 setIsLoading(false);
               }
             }
-          } catch(err){
-            setError('No Records Found')
+          } catch (err) {
+            setError("No Records Found");
             await signOut(auth);
             setIsLoading(false);
           }
@@ -72,30 +70,46 @@ const SignIn = () => {
     }
   };
 
-  const isFormValid = email.trim() !== '' && password.trim() !== '';
+  const isFormValid = email.trim() !== "" && password.trim() !== "";
 
   return (
     <main className={styles.pageContainer}>
       <nav className={styles.navBar}>
         <Link href="/">
-          <MdArrowBackIosNew style={{ fontSize: '20px' }} />
+          <MdArrowBackIosNew style={{ fontSize: "20px" }} />
         </Link>
       </nav>
       <div className={styles.formContainer}>
-        <div style={{ display: 'flex', justifyContent: 'center', flexDirection: 'row' }}>
-          <img src="/logo.png" style={{ maxWidth: '60px', maxHeight: '60px', alignItems: 'center' }} />
-          <h1 style={{ fontSize: '25px', padding: '10px' }}> | </h1>
-          <img src="/logorv.png" style={{ maxWidth: '60px', maxHeight: '60px', alignItems: 'center' }} />
+        <div
+          style={{
+            display: "flex",
+            justifyContent: "center",
+            flexDirection: "row",
+          }}
+        >
+          <Image alt="LOGO" src="/logo.png" width={60} height={60} />
+          <h1 style={{ fontSize: "25px", padding: "10px" }}> | </h1>
+          <Image alt="CollegeLOGO" src="/logorv.png" width={60} height={60} />
         </div>
-        <div style={{ textAlign: 'center', margin: '25px' }}>
-          <h1 style={{ fontWeight: '600', fontFamily: 'Poppins', fontSize: '1.5rem' }}>Welcome!</h1>
-          <p style={{ color: '#030303', fontSize: '14px' }}>Please Sign In to your account to Continue</p>
+        <div style={{ textAlign: "center", margin: "25px" }}>
+          <h1
+            style={{
+              fontWeight: "600",
+              fontSize: "1.5rem",
+            }}
+          >
+            Welcome!
+          </h1>
+          <p style={{ color: "#030303", fontSize: "14px" }}>
+            Please Sign In to your account to Continue
+          </p>
         </div>
         <div className={styles.inputHeading}>Email Address</div>
         <div className={styles.inputContainer}>
           <input
             type="email"
             id="email"
+            autocomplete="email"
             placeholder="Enter your email"
             className={styles.formInput}
             value={email}
@@ -111,13 +125,17 @@ const SignIn = () => {
           <input
             placeholder="Enter your password"
             className={styles.formInput}
-            type={passwordShown ? 'text' : 'password'}
+            type={passwordShown ? "text" : "password"}
             id="password"
+            autocomplete="current-password"
             value={password}
             onChange={(e) => setPassword(e.target.value)}
             required // Add the required attribute
           />
-          <span onClick={togglePasswordVisibility} className={styles.passwordButton}>
+          <span
+            onClick={togglePasswordVisibility}
+            className={styles.passwordButton}
+          >
             {passwordShown ? <FiEye /> : <FiEyeOff />}
           </span>
           <div className={styles.iconContainer}>
@@ -125,20 +143,45 @@ const SignIn = () => {
           </div>
         </div>
         <div className={styles.forgotPassword}>Forgot Password?</div>
-        {error && <Alert variant="danger" style={{ fontSize: '12px' }}>{error}</Alert>}
-        <button className={styles.primaryButton} onClick={handleSubmit} disabled={isLoading || !isFormValid}>
-          {isLoading ? 'Loading...' : 'Sign In'}
+        {error && (
+          <Alert variant="danger" style={{ fontSize: "12px" }}>
+            {error}
+          </Alert>
+        )}
+        <button
+          className={styles.primaryButton}
+          onClick={handleSubmit}
+          disabled={isLoading || !isFormValid}
+        >
+          {isLoading ? "Loading..." : "Sign In"}
         </button>
-        <p style={{ color: '#030303', fontSize: '14px', marginTop: '15px', fontWeight: '500', color: '#333' }}>
-          Account not activated? <Link href="/" style={{ color: 'blue' }}>Click here</Link>
+        <p
+          style={{
+            color: "#030303",
+            fontSize: "14px",
+            marginTop: "15px",
+            fontWeight: "500",
+            color: "#333",
+          }}
+        >
+          Account not activated?
+          <Link href="/" style={{ color: "blue" }}>
+            Click here
+          </Link>
         </p>
         <div className={styles.or}>
           <hr className={styles.bar} />
-          <span className="mx-3">OR</span>
+          <span className="mx-3"> OR </span>
           <hr className={styles.bar} />
         </div>
         <button className={styles.googleButton}>
-          <img src="https://www.gstatic.com/firebasejs/ui/2.0.0/images/auth/google.svg" alt="Google Logo"></img> Continue with Google
+          <Image
+            src="https://www.gstatic.com/firebasejs/ui/2.0.0/images/auth/google.svg"
+            alt="Google Logo"
+            width={60}
+            height={60}
+          ></Image>
+          Continue with Google
         </button>
       </div>
     </main>
--- app/page.jsx
+++ app/page.jsx
@@ -1,27 +1,56 @@
-'use client';
-import React from 'react';
-import styles from './HomePage.module.css';
-import Link from 'next/link';
+"use client";
+import React from "react";
+import styles from "./HomePage.module.css";
+import Link from "next/link";
+import Image from "next/image";
 
 const HomePage = () => {
   return (
-      <main>
-            <div className={styles.pageContainer}>              
-              <div style={{ display: 'flex', justifyContent: 'center', flexDirection: 'row' }}>
-                <img src='/logo.png' style={{ maxWidth: '60px', maxHeight: '60px', alignItems: 'center' }} />
-                <h1 style={{ fontSize: '25px', padding: '10px' }}> | </h1>
-                <img src='/logorv.png' style={{ maxWidth: '60px', maxHeight: '60px', alignItems: 'center' }} />
-              </div>
-              <h1 style={{ fontSize: '30px', fontWeight: 'bolder', margin: '20px', fontFamily: 'golos text', fontWeight: '600' }}>Welcome to EduStack for RVITM</h1>
-              <p style={{ margin: '20px', color: 'grey', fontFamily: 'sans-serif', marginBottom: '15vh'}}>A Platform Built to Simplify Attendance Tracking & Academics </p>
+    <main>
+      <div className={styles.pageContainer}>
+        <div
+          style={{
+            display: "flex",
+            justifyContent: "center",
+            flexDirection: "row",
+          }}
+        >
+          <Image alt="LOGO" src="/logo.png" width={60} height={60} />
+          <h1 style={{ fontSize: "25px", padding: "10px" }}> | </h1>
+          <Image alt="CollegeLOGO" src="/logorv.png" width={60} height={60} />
+        </div>
+        <h1
+          style={{
+            fontSize: "30px",
+            fontWeight: "bolder",
+            margin: "20px",
+            fontWeight: "600",
+          }}
+        >
+          Welcome to EduStack for RVITM
+        </h1>
+        <p
+          style={{
+            margin: "20px",
+            color: "grey",
+            fontFamily: "sans-serif",
+            marginBottom: "15vh",
+          }}
+        >
+          A Platform Built to Simplify Attendance Tracking & Academics{" "}
+        </p>
 
-              <div className={styles.buttonContainer}>
-              <Link href='/accounts/signin' style={{width: '95%'}}><button className={styles.primaryButton}>Sign In </button></Link>
-              <button className={styles.secondaryButton}>Activate your Account</button>
-            </div>
-            </div>
-      </main>
+        <div className={styles.buttonContainer}>
+          <Link href="/accounts/signin" style={{ width: "95%" }}>
+            <button className={styles.primaryButton}>Sign In </button>
+          </Link>
+          <button className={styles.secondaryButton}>
+            Activate your Account
+          </button>
+        </div>
+      </div>
+    </main>
   );
-}
+};
 
 export default HomePage;
